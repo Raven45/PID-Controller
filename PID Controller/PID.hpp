eb1cd1ad47fafaa470ecdd182ddca66f46e50879
audio_000 <audio_000@AARON-PC>
2016-09-03 16:33:25 -0500

    9/3/2016: Added documentation

--- PID Controller/PID.hpp
+++ PID Controller/PID.hpp
@@ -10,21 +10,78 @@ namespace ControlLib {
 	class PID {
 
 	public:
+		/**********************************************************************
+		Class Constructors
+		**********************************************************************/
 		PID ();
 		PID (float P);
 		PID (float P, float I);
 		PID (float P, float I, float D);
 
+		/**********************************************************************
+		Function name: SetKp
+		Input:		Kp (float): the specified gain for the internal proportional 
+					controller.
+		Outputs:	None.
+		Description: Sets the gain for the internal proportional controller to
+		the specified input Kp (float).
+		**********************************************************************/
 		void SetKp (float Kp);
+
+		/**********************************************************************
+		Function name: SetKi
+		Input:		Ki (float): the specified gain for the internal integral
+					controller.
+		Outputs:	None.
+		Description: Sets the gain for the internal integral controller to
+		the specified input Ki (float).
+		**********************************************************************/
 		void SetKi (float Ki);
+
+		/**********************************************************************
+		Function name: SetKd
+		Input:		Kd (float): the specified gain for the internal derivative
+		controller.
+		Outputs:	None.
+		Description: Sets the gain for the internal derivative controller to
+		the specified input Kd (float).
+		**********************************************************************/
 		void SetKd (float Kd);
 		void SetZeroPoint (DataType ZeroPoint);
 
+		/**********************************************************************
+		Function name: GetKp
+		Input:		None.
+		Outputs:	The proportional gain of the internal P controller.
+		**********************************************************************/
 		float GetKp () const;
+
+		/**********************************************************************
+		Function name: GetKi
+		Input:		None.
+		Outputs:	The proportional gain of the internal I controller.
+		**********************************************************************/
 		float GetKi () const;
+
+		/**********************************************************************
+		Function name: GetKd
+		Input:		None.
+		Outputs:	The proportional gain of the internal D controller.
+		**********************************************************************/
 		float GetKd () const;
 		DataType GetZeroPoint () const;
 
+		/**********************************************************************
+		Function name: Update
+		Input:		Error (DataType): The error point for the PID controller.
+					DeltaTime (unsigned int): The amount of time since the
+						last time the Update function was called. 
+		Outputs:	Output from the PID controller.
+		Description: This will take an error and the amount of time since the
+			last run and calculate the output of the internal PID controller.
+			The error needs to be pre-calculated before being passed to the PID
+			controller.
+		**********************************************************************/
 		DataType Update (DataType Error, unsigned int DeltaTime);
 
 	protected:
@@ -35,8 +92,31 @@ namespace ControlLib {
 		DataType SumErrors;
 		DataType ZeroPoint;
 
+		/**********************************************************************
+		Function name: CalculateP
+		Input:		Error (DataType): The error point for the PID controller.
+		Outputs:	Output from the proportional controller.
+		Description: Will take a pre-calculated error and calculate the output
+			of the internal proportional controller.
+		**********************************************************************/
 		DataType CalculateP (DataType Error);
+
+		/**********************************************************************
+		Function name: CalculateI
+		Input:		Error (DataType): The error point for the PID controller.
+		Outputs:	Output from the integral controller.
+		Description: Will take a pre-calculated error and calculate the output
+		of the internal integral controller.
+		**********************************************************************/
 		DataType CalculateI (DataType Error, unsigned int DeltaTime);
+
+		/**********************************************************************
+		Function name: CalculateD
+		Input:		Error (DataType): The error point for the PID controller.
+		Outputs:	Output from the derivative controller.
+		Description: Will take a pre-calculated error and calculate the output
+		of the internal derivative controller.
+		**********************************************************************/
 		DataType CalculateD (DataType Error, unsigned int DeltaTime);
 	};
 
@@ -108,18 +188,46 @@ namespace ControlLib {
 		return this->ZeroPoint;
 	}
 
+	/**********************************************************************
+	Function name: Update
+	Input:		Error (DataType): The error point for the PID controller.
+	DeltaTime (unsigned int): The amount of time since the
+	last time the Update function was called.
+	Outputs:	Output from the PID controller.
+	Description: This will take an error and the amount of time since the
+	last run and calculate the output of the internal PID controller.
+	The error needs to be pre-calculated before being passed to the PID
+	controller.
+	History:	9/3/2016: Initial Development
+	**********************************************************************/
 	template<class DataType>
 	inline DataType PID<DataType>::Update (DataType Error, unsigned int DeltaTime) {
 		
 		return CalculateP (Error) + CalculateD (Error, DeltaTime) + CalculateI (Error, DeltaTime);
 	}
 
+	/**********************************************************************
+	Function name: CalculateP
+	Input:		Error (DataType): The error point for the PID controller.
+	Outputs:	Output from the proportional controller.
+	Description: Will take a pre-calculated error and calculate the output
+	of the internal proportional controller.
+	History:	9/3/2016: Initial Development
+	**********************************************************************/
 	template<class DataType>
 	inline DataType PID<DataType>::CalculateP (DataType Error) {
 		
 		return Kp * Error;
 	}
 
+	/**********************************************************************
+	Function name: CalculateI
+	Input:		Error (DataType): The error point for the PID controller.
+	Outputs:	Output from the integral controller.
+	Description:	Will take a pre-calculated error and calculate the output
+					of the internal integral controller.
+	History:	9/3/2016: Initial Development.
+	**********************************************************************/
 	template<class DataType>
 	inline DataType PID<DataType>::CalculateI (DataType Error, unsigned int DeltaTime) {
 
@@ -130,6 +238,14 @@ namespace ControlLib {
 		return SumErrors * DeltaTime;
 	}
 
+	/**********************************************************************
+	Function name: CalculateD
+	Input:		Error (DataType): The error point for the PID controller.
+	Outputs:	Output from the derivative controller.
+	Description: Will take a pre-calculated error and calculate the output
+	of the internal derivative controller.
+	History:	9/3/2016: Initial Development.
+	**********************************************************************/
 	template<class DataType>
 	inline DataType PID<DataType>::CalculateD (DataType Error, unsigned int DeltaTime) {
 		
